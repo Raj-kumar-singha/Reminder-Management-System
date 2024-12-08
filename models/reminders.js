de8739e1676314f521dbcc9d229d9fba0b4cf5bc
Raj <itsrajkumarsingha@gmail.com>
2024-12-09 00:26:05 +0530

    adding timestamp in schema

--- models/reminders.js
+++ models/reminders.js
@@ -1,16 +1,23 @@
 const mongoose = require('mongoose');
 
-const reminderSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  message: { type: String, required: true },
-  time: { type: Date, required: true },
-  recurrence: {
-    type: String,
-    enum: ['daily', 'weekly', 'monthly'],
-    default: null,
+const reminderSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, unique: true },
+    message: { type: String, required: true },
+    time: { type: Date, required: true },
+    recurrence: {
+      type: String,
+      enum: ['daily', 'weekly', 'monthly'],
+      default: null,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'triggered'],
+      default: 'pending',
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   },
-  status: { type: String, enum: ['pending', 'triggered'], default: 'pending' },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Reminder', reminderSchema);
